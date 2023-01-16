35e832a8c6cfdfc85aefc5c97e3d782529e4a97f
KritarthChavda <113841682+KritarthChavda@users.noreply.github.com>
2023-01-16 18:40:08 +0530

    Update day7.js

--- day7.js
+++ day7.js
@@ -1,40 +1,75 @@
-/*
-Ilya and Bank Account
-time limit per test
-2 seconds
-memory limit per test
-256 megabytes
-input
-standard input
-output
-standard output
-Ilya is a very clever lion, he lives in an unusual city ZooVille. In this city all the animals have their rights and obligations. Moreover, they even have their own bank accounts. The state of a bank account is an integer. The state of a bank account can be a negative number. This means that the owner of the account owes the bank money.
-Ilya the Lion has recently had a birthday, so he got a lot of gifts. One of them (the gift of the main ZooVille bank) is the opportunity to delete the last digit or the digit before last from the state of his bank account no more than once. For example, if the state of Ilya's bank account is -123, then Ilya can delete the last digit and get his account balance equal to -12, also he can remove its digit before last and get the account balance equal to -13. Of course, Ilya is permitted not to use the opportunity to delete a digit from the balance.
-Ilya is not very good at math, and that's why he asks you to help him maximize his bank account. Find the maximum state of the bank account that can be obtained using the bank's gift.
+/*A company of n
+ people is planning a visit to the cinema. Every person can either go to the cinema or not. That depends on how many other people will go. Specifically, every person i
+ said: "I want to go to the cinema if and only if at least ai
+ other people will go, not counting myself". That means that person i
+ will become sad if:
+
+they go to the cinema, and strictly less than ai
+ other people go; or
+they don't go to the cinema, and at least ai
+ other people go.
+In how many ways can a set of people going to the cinema be chosen so that nobody becomes sad?
+
 Input
-The single line contains integer n (10 ≤ |n| ≤ 109) — the state of Ilya's bank account.
+Each test contains multiple test cases. The first line contains the number of test cases t
+ (1≤t≤104
+). The description of the test cases follows.
+
+Each test case consists of two lines. The first line contains a single integer n
+ (2≤n≤2⋅105
+) — the number of people in the company.
+
+The second line contains n
+ integers a1,a2,…,an
+ (0≤ai≤n−1
+) — integers from peoples' claims.
+
+It is guaranteed that the sum of n
+ over all test cases does not exceed 2⋅105
+.
+
 Output
-In a single line print an integer — the maximum state of the bank account that Ilya can get.
-Examples
-Input
-2230
-Output
-2230
-Input
--10
-Output
-0
-Input
--100003
-Output
--10000
+For each test case, print a single integer — the number of different ways to choose a set of people going to the cinema so that nobody becomes sad.
+
+Example
+inputCopy
+4
+2
+1 1
+7
+0 1 2 3 4 5 6
+8
+6 0 3 3 6 7 2 7
+5
+3 0 0 3 3
+outputCopy
+2
+1
+3
+2
 */
-var s = readline();
-var res = parseInt(s);
-if (! (s['0'] == '-' && s.length <= 2)) {
-  var r = parseInt(s.slice(0, s.length - 1));
-  res = Math.max(res, r)
-  var r2 = parseInt(s.slice(0, s.length - 2) + s[s.length - 1]);
-  res = Math.max(res, r2)
+
+var numTestCases = parseInt(readline()); // reading num test cases
+ 
+for (var testCaseCount = 0; testCaseCount < numTestCases; testCaseCount++) {
+    var numEmployees = parseInt(readline());
+    var employeeClaimArray = readline().split(" ").map(value => parseInt(value));
+ 
+    employeeClaimArray.sort((a,b) => (parseInt(a) - parseInt(b)));
+ 
+    var setCount = 0;
+    for(var index = 0 ; index < (employeeClaimArray.length - 1); index++){
+        if(employeeClaimArray[index] <= index && employeeClaimArray[index + 1] > (index + 1)){
+            setCount++;
+        }
+    }
+ 
+    // adding 1 for no one
+    if(employeeClaimArray[0] != 0){
+        setCount++;
+    }
+ 
+    // adding 1 for everyone
+    setCount++;
+    print(setCount);
 }
-print(res);
