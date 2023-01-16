744f13c8883147bbda77c478023ec33851b2c57a
KritarthChavda <113841682+KritarthChavda@users.noreply.github.com>
2023-01-16 11:36:52 +0530

    Update day6.js

--- day6.js
+++ day6.js
@@ -1,31 +1,39 @@
-/*A string a=a1a2…an is called even if it consists of a concatenation (joining) of strings of length 2 consisting of the same characters. In other words, a string a is even if two conditions are satisfied at the same time:
-its length n is even;
-for all odd i (1≤i≤n−1), ai=ai+1 is satisfied.
-For example, the following strings are even: "" (empty string), "tt", "aabb", "oooo", and "ttrrrroouuuuuuuukk". The following strings are not even: "aaa", "abab" and "abba".
-Given a string s consisting of lowercase Latin letters. Find the minimum number of characters to remove from the string s to make it even. The deleted characters do not have to be consecutive.
+/*Vika's house has a room in a shape of a rectangular parallelepiped (also known as a rectangular cuboid). Its floor is a rectangle of size w×d, and the ceiling is right above at the constant height of h. Let's introduce a coordinate system on the floor so that its corners are at points (0,0), (w,0), (w,d), and (0,d).
+
+A laptop is standing on the floor at point (a,b). A projector is hanging on the ceiling right above point (f,g). Vika wants to connect the laptop and the projector with a cable in such a way that the cable always goes along the walls, ceiling, or floor (i. e. does not go inside the cuboid). Additionally, the cable should always run parallel to one of the cuboid's edges (i. e. it can not go diagonally).
+
+What is the minimum length of a cable that can connect the laptop to the projector?
+
+Illustration for the first test case. One of the optimal ways to put the cable is shown in green.
 Input
-The first line of input data contains an integer t (1≤t≤104) —the number of test cases in the test.
-The descriptions of the test cases follow.
-Each test case consists of one string s (1≤|s|≤2⋅105), where |s| — the length of the string s. The string consists of lowercase Latin letters.
-It is guaranteed that the sum of |s| on all test cases does not exceed 2⋅105.
+Each test contains multiple test cases. The first line contains the number of test cases t (1≤t≤104). The description of the test cases follows.
+
+The first line of each test case contains three integers w, d, and h (2≤w,d,h≤1000) — the size of the room.
+
+The second line contains four integers a, b, f, g (0<a,f<w; 0<b,g<d): the laptop is located on the floor at point (a,b), while the projector is hanging on the ceiling right above point (f,g).
+
 Output
-For each test case, print a single number — the minimum number of characters that must be removed to make s even.
+For each test case, print a single integer — the minimum length of the cable connecting the laptop and the projector that runs only along the walls, floor, and ceiling parallel to cuboid's edges.
+
 Example
-input
-6
-aabbdabdccc
-zyx
-aaababbb
-aabbcc
-oaoaaaoo
-bmefbmuyw
-output
-3
-3
-2
-0
-2
-7
+inputCopy
+5
+55 20 29
+23 10 18 3
+20 10 5
+1 5 2 5
+15 15 4
+7 13 10 10
+2 1000 2
+1 1 1 999
+10 4 10
+7 1 2 1
+outputCopy
+47
+8
+14
+1002
+17
 */
 
 var n = readline();
