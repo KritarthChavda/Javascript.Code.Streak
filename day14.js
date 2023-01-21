7d99547bbac82854d1457ce664ac27401d4ff930
KritarthChavda <113841682+KritarthChavda@users.noreply.github.com>
2023-01-21 23:30:08 +0530

    Add files via upload

---
+++ day14.js
@@ -0,0 +1,24 @@
+var numTestCases = parseInt(readline()); // reading num test cases
+ 
+for (var testCaseCount = 0; testCaseCount < numTestCases; testCaseCount++) {
+    var numEmployees = parseInt(readline());
+    var employeeClaimArray = readline().split(" ").map(value => parseInt(value));
+ 
+    employeeClaimArray.sort((a,b) => (parseInt(a) - parseInt(b)));
+ 
+    var setCount = 0;
+    for(var index = 0 ; index < (employeeClaimArray.length - 1); index++){
+        if(employeeClaimArray[index] <= index && employeeClaimArray[index + 1] > (index + 1)){
+            setCount++;
+        }
+    }
+ 
+    // adding 1 for no one
+    if(employeeClaimArray[0] != 0){
+        setCount++;
+    }
+ 
+    // adding 1 for everyone
+    setCount++;
+    print(setCount);
+}
